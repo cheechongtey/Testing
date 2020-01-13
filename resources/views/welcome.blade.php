b734295d9c4956cd6ce192a95856b2f78fe38b4e
davischoo <tower9797@gmail.com>
2020-01-13 15:17:53 +0800

    CC add home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,8 +85,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel.com/docs">Hello boi</a>
+                    <a href="https://laracasts.com">Bye Boi</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">bla bla bla</a>
                     <a href="https://nova.laravel.com">Nova</a>
