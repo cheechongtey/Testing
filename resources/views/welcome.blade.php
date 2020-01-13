8d78423e75c0c283aee8f6d0574856d7480c4c32
davischoo <tower9797@gmail.com>
2020-01-13 14:53:38 +0800

    CC add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,8 +85,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel.com/docs">Worry</a>
+                    <a href="https://laracasts.com">Bla bla bla</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
